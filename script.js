39ed6ef39944fa03a72c326106a4e3f82ef52b78
nangstywn <setyawannanang37@gmail.com>
2022-07-17 19:53:26 +0700

    add scrollIndicator

--- script.js
+++ script.js
@@ -91,3 +91,44 @@ if(scrollButton){
         window.scrollTo(0, 0);
     });
 }
+
+window.onload = function() {
+  const container = document.querySelector('.indicator');
+  const svg = document.querySelector('svg');
+  const progressBar = document.querySelector('.progress-bar');
+  const pct = document.querySelector('.pct');
+  const totalLength = progressBar.getTotalLength();
+    
+  setTopValue(svg);
+  
+  progressBar.style.strokeDasharray = totalLength;
+  progressBar.style.strokeDashoffset = totalLength;
+  
+  window.addEventListener('scroll', () => {
+    setProgress(container, pct, progressBar, totalLength);
+  });
+  
+  window.addEventListener('resize', () => {
+    setTopValue(svg);
+  });
+}
+
+function setTopValue(svg) {
+  svg.style.top = document.documentElement.clientHeight * 0.5 - (svg.getBoundingClientRect().height * 0.5) + 'px';
+}
+
+
+function setProgress(container, pct, progressBar, totalLength) {
+  const clientHeight = document.documentElement.clientHeight;
+  const scrollHeight = document.documentElement.scrollHeight;
+  const scrollTop = document.documentElement.scrollTop;
+  
+  const percentage = scrollTop / (scrollHeight - clientHeight);
+  if(percentage === 1) {
+    container.classList.add('completed');
+  } else {
+    container.classList.remove('completed');
+  }
+  pct.innerHTML = Math.round(percentage * 100) + '%';
+  progressBar.style.strokeDashoffset = totalLength - totalLength * percentage;  
+}
